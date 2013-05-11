17d5f8798861ad02261e1bb1786f379666749732
Tim Chaplin <tjchaplin@hotmail.com>
2013-05-11 14:01:32 +0100

    updated commit request to handle github api error codes

--- tests/integrationTests.js
+++ tests/integrationTests.js
@@ -1,66 +1,66 @@
-// var should = require('should');
-// var gitHubCommits = require("../lib/github-commits.js");
-// var enumerable = require("yaenumerable");
+var should = require('should');
+var gitHubCommits = require("../lib/github-commits.js");
+var enumerable = require("yaenumerable");
 
-// var fs = require('fs');
+var fs = require('fs');
 
-// describe('When using git api',function(){
-// 	this.timeout(60000);
-// 	it("should be able to make a request",function(onComplete){
-// 		var gitConnection = gitHubCommits.Connect();
-// 		gitConnection.makeRequest("https://api.github.com/users/tjchaplin",function(data){
-// 			data.should.not.equal(undefined);
-// 			onComplete();
-// 		});
-// 	});
+describe('When using git api',function(){
+	this.timeout(60000);
+	it("should be able to make a request",function(onComplete){
+		var gitConnection = gitHubCommits.Connect();
+		gitConnection.makeRequest("https://api.github.com/users/tjchaplin",function(data){
+			data.should.not.equal(undefined);
+			onComplete();
+		});
+	});
 	
-// 	it("should be able to get repositories based on a username",function(onComplete){
-// 		var gitConnection = gitHubCommits.Connect();
-// 		gitConnection.getRepositories("tjchaplin",function(data){
-// 			data.should.not.equal(undefined);
-// 			onComplete();			
-// 		});
-// 	});
+	it("should be able to get repositories based on a username",function(onComplete){
+		var gitConnection = gitHubCommits.Connect();
+		gitConnection.getRepositories("tjchaplin",function(data){
+			data.should.not.equal(undefined);
+			onComplete();			
+		});
+	});
 	
-// 	it("should be able to get repositories based on a user",function(onComplete){
-// 		var gitConnection = gitHubCommits.Connect();
-// 		gitConnection.getRepositories({name:"tjchaplin",type:"users"},function(data){
-// 			data.should.not.equal(undefined);
-// 			onComplete();			
-// 		});
-// 	});	
+	it("should be able to get repositories based on a user",function(onComplete){
+		var gitConnection = gitHubCommits.Connect();
+		gitConnection.getRepositories({name:"tjchaplin",type:"users"},function(data){
+			data.should.not.equal(undefined);
+			onComplete();			
+		});
+	});	
 	
-// 	it("should be able to get number of repository commits",function(onComplete){
-// 		var gitConnection = gitHubCommits.Connect();
-// 		gitConnection.updateRepositoryCommitCount({name:"developer.github.com",owner:"github"},{},function(data){
-// 			data.numberOfCommits.should.be.above(-1);
-// 			onComplete();			
-// 		});
-// 	});
+	it("should be able to get number of repository commits",function(onComplete){
+		var gitConnection = gitHubCommits.Connect();
+		gitConnection.updateRepositoryCommitCount({name:"developer.github.com",owner:"github"},{},function(data){
+			data.numberOfCommits.should.be.above(-1);
+			onComplete();			
+		});
+	});
 
-// 	it("should be able to total commits for all user repositories",function(onComplete){
-// 		var gitConnection = gitHubCommits.Connect();
-// 		gitConnection.getAllRepositoryCommits({name:"tjchaplin",type:"users"},{},function(repositories){
-// 			repositories[0].numberOfCommits.should.be.above(-1);
-// 			onComplete();			
-// 		});
-// 	});
+	it("should be able to total commits for all user repositories",function(onComplete){
+		var gitConnection = gitHubCommits.Connect();
+		gitConnection.getAllRepositoryCommits({name:"tjchaplin",type:"users"},{},function(repositories){
+			repositories[0].numberOfCommits.should.be.above(-1);
+			onComplete();			
+		});
+	});
 
-// 	it("should be able to filter commits by a start and end date",function(onComplete){
-// 		var sinceDate = new Date("2013-05-04T00:00:00Z");
-// 		var untilDate = new Date("2013-05-06T23:59:59Z");
-// 		var gitConnection = gitHubCommits.Connect();
-// 		var optionFilters = {sinceDate:sinceDate,untilDate : untilDate};
-// 		var owner = {name:"tjchaplin",type:"users"}
-// 		gitConnection.getAllRepositoryCommits(owner,optionFilters,function(repositories){
-// 			var result = enumerable.FromArray(repositories)
-// 								.Where(function(repository){return repository.name == "YAEnumerable"})
-// 								.First();
+	it("should be able to sum all commits when filtered commits by a start and end date",function(onComplete){
+		var sinceDate = new Date("2013-05-05T00:00:00Z");
+		var untilDate = new Date("2013-05-05T23:59:59Z");
+		var gitConnection = gitHubCommits.Connect();
+		var optionFilters = {sinceDate:sinceDate,untilDate : untilDate};
+		var owner = {name:"tjchaplin",type:"users"}
+		gitConnection.getAllRepositoryCommits(owner,optionFilters,function(repositories){
+			var result = enumerable.FromArray(repositories)
+								.Where(function(repository){return repository.name == "YAEnumerable"})
+								.First();
 
-// 			result.numberOfCommits.should.be.equal(19);
-// 			onComplete();			
-// 		});
-// 	});
+			result.numberOfCommits.should.be.equal(18);
+			onComplete();			
+		});
+	});
 
 // 	//-----------------------------
 // 	//Explict org tests
@@ -80,5 +80,6 @@
 // 	// 		onComplete();			
 // 	// 	});
 // 	// });
-// });
+
+});
 
